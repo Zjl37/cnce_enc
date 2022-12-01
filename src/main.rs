97800d27df8f72ffd38de569f3983bc71358215d
Zjl37 <2693911885@qq.com>
2022-12-01 13:05:08 +0800

    向stderr写错误信息

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ use cnce_enc::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("解析参数出错: {err}");
+        eprintln!("解析参数出错: {err}");
         process::exit(1);
     });
 //     println!("=== 文件名：{} ===", config.file_path);
 
     if let Err(e) = cnce_enc::run(config) {
-        println!("出错了: {e}");
+        eprintln!("出错了: {e}");
         process::exit(1);
     }
 }
